0b831b1dca1eb12d5b5dbe99daaee8550677bc13
Victor Ferreira <victoreferreirag@gmail.com>
2024-05-21 18:48:59 -0300

    Acrescentando a ação de puxar pra baixo e dar refresh.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,18 +10,59 @@ import { LoadingController } from '@ionic/angular';
 })
 
 export class Tab2Page {
-  
   listaFilmes: any[] = [];
+  public page: number = 1;
 
-  constructor( public theMoviedbService:ThemoviedbService) {
-    this.theMoviedbService.getPopularMovies(1, 'pt').subscribe({next: (data:any) => {
-      const response = data;
-      this.listaFilmes = this.listaFilmes.concat(response.results);
-      console.log(this.listaFilmes);
-    }, error: (error:any) => {
-      console.log('Erro ao carregar filmes:', error);
-    }
-  });
+  constructor(
+    public theMoviedbService: ThemoviedbService,
+    public loadingController: LoadingController,
+  ){
+    // Carrega filmes ao inicializar o componente
+    this.carregarFilmes();
+  }
   
+  // Método feito carregar filmes da API.
+  async carregarFilmes() {
+    const loading = await this.loadingController.create({
+      message: 'Carregando filmes',
+      spinner: 'lines',
+    });
+    await loading.present();
+
+     // Chama o serviço para obter os filmes.
+    this.theMoviedbService.getPopularMovies(this.page, 'pt').subscribe({
+      next: (data: any) => {
+        const response = data;
+        this.listaFilmes = response.results;
+        console.log(this.listaFilmes);
+        loading.dismiss();
+      },
+      error: (error: any) => {
+        console.log('Erro ao carregar filmes:', error);
+        loading.dismiss();
+      }
+    });
+  }
+
+  // Método para definir um spinner por um tempo definido.
+  async efeitoLoading() {
+    const loading = await this.loadingController.create({
+      message: 'Carregando filmes',
+      duration: 1000,
+      spinner: 'lines',
+    });
+    await loading.present();
+    await loading.onDidDismiss();
+  }
+
+  ionViewDidEnter() {
+    this.carregarFilmes();
+  }
+  //Ação de refresh puxando para baixo.
+  async efeitoRefresh(event: any) {
+    this.page = 1;
+    await this.carregarFilmes();
+    event.target.complete();
+    console.log("Finalizando refresh");
   }
 }

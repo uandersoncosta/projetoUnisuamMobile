ba1f3193c7260732b77685cf2ce10487c6e21109
PatrickSxavier7 <patrickssantos324@gmail.com>
2024-06-15 18:17:39 -0300

    config infos

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
-  constructor() { }
+  regForm: FormGroup | undefined;
+  constructor(public formBuilder:FormBuilder) { }
 
   ngOnInit() {
-  }
+    this.regForm = this.formBuilder.group({
+      fullname :['', [Validators.required]],
+      email :['', [Validators.required,
+                   Validators.email,
+                   Validators.pattern("[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"),
+      ]],
+      password:['',
+        Validators.required,
+        Validators.pattern("(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")
+      ]
+    })
+}
 
+get errorControl(){
+  return this.regForm?.controls;
+}
 }
